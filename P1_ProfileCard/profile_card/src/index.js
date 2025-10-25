defd5c26206315db31b3f0983ba8f1b5e2c17c0e
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-10-25 07:04:46 +0530

    Profile card mini project v2

--- P1_ProfileCard/profile_card/src/index.js
+++ P1_ProfileCard/profile_card/src/index.js
@@ -2,41 +2,84 @@ import React, { StrictMode } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import "./styles.css";
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
 const App = () => {
   return (
-    <div><Card  />
+    <div>
+      <Card />
     </div>
   );
 };
 const Card = () => {
   return (
-    <div className="card data" >
+    <div className="card data">
       <img src="Person.jpg" alt="Photo of a Person" className="avatar" />
       <h1 className="h1">Nitigya Chandel</h1>
-      <p>Hello everyone my name is Nitigya Chandel. Currently I am a student at IIIT Ranchi and i aspire to be a software developer.</p>
-      <SkillList/>
+      <p>
+        Hello everyone my name is Nitigya Chandel. Currently I am a student at
+        IIIT Ranchi and i aspire to be a software developer.
+      </p>
+      <SkillList />
     </div>
   );
 };
 
-const SkillList = ()=>{
+const SkillList = () => {
   return (
     <div className="skill-list">
-      <Skill skillName="JavaScript" skillEmoji="😁" color="red"/>
-      <Skill skillName="C++" skillEmoji="💪" color="blue" />
-      <Skill skillName="React" skillEmoji="👍" color="yellow"/>
-      <Skill skillName="Node" skillEmoji="👌" color="green" />
+      {skills.map((skill) => (
+        <Skill skillObj={skill} />
+      ))}
     </div>
   );
-}
+};
 
-const Skill = (props)=>{
+const Skill = ({ skillObj }) => {
   return (
-    <div className="skill" style={{backgroundColor:props.color}}>
-      <h4>{props.skillName}{props.skillEmoji}</h4>
+    <div className="skill" style={{ backgroundColor: skillObj.color }}>
+      <h4>
+        {skillObj.skill}
+        {
+          skillObj.level=="advanced"?
+          <span>💪</span>:skillObj.level=="intermediate"?
+          <span>👍</span>:<span>👶</span>
+        }
+      </h4>
     </div>
   );
-}
+};
 
 const root = createRoot(document.getElementById("root"));
 root.render(
